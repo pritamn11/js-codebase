c029f5166bf30b08253ce31eb67b2f5c411fde7a
Pritam Narwade <118045878+Pritamn11@users.noreply.github.com>
2024-01-29 08:22:04 +0000

    conversion operation

---
+++ 01_basics/03_conversion_Operation.js
@@ -0,0 +1,31 @@
+let score = "43"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+// "43" => 43
+// "43abc" => Nan
+// true => 1, false => 0
+
+
+let isLoggedIn = "pritam"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true, 0 => false 
+// "" => false
+// "pritam" => true
+
+
+let someNumber = 33 
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
