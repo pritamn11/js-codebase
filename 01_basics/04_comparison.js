69170c220c1cb12458ad36912c0108dd3adcae70
Pritamn11 <pritamnarwade11@gmail.com>
2024-01-30 14:07:22 +0530

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,19 @@
+// console.log(2 > 1)
+// console.log(2 < 1)
+// console.log(1 <= 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+
+// console.log("2" > 1)  // true
+// console.log(2 > "01")  // true
+
+// console.log(null > 0) // false
+// console.log(null == 0)  // false
+// console.log(null >= 0)  // true 
+// console.log(null < 1)  // true 
+
+// console.log(undefined >= 0)  // false
+// console.log(undefined < 0)  // false
+// console.log(undefined > 0)  // false
+
