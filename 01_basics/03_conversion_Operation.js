09ec5ed782ab4fd77fb2c9725e14fcd0c8623349
Pritamn11 <pritamnarwade11@gmail.com>
2024-01-30 13:39:00 +0530

    Operation Conversion

--- 01_basics/03_conversion_Operation.js
+++ 01_basics/03_conversion_Operation.js
@@ -27,5 +27,48 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33 
 
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber)
-console.log(stringNumber)
+// console.log(typeof stringNumber)
+// console.log(stringNumber)
+
+
+// *********************** OPERATIONS ****************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)  // -3
+
+let str1 = "Hello"
+let str2 = " Pritam"
+let str3 = str1 + str2 
+// console.log(str3)
+
+// console.log(1 + "2")
+// console.log("1" + 2)
+// console.log(typeof("1" + 2))
+// console.log("1" + 2 + 2)  // 122 
+// console.log(1 + 2 + "2")  // 32
+
+
+// console.log(true) // true
+// console.log(+true) // 1
+// console.log(true+) // Throw Error 
+
+
+let gameScore = 100
+++gameScore
+// console.log(gameScore)
+
+
+let x = 3;
+let y = x++;
+
+console.log(`x:${x}, y:${y}`);
+
+/*
+If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing.
+
+If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.
+*/
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
