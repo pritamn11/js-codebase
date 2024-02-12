3b104491f1789bb7616c81ecc6e52e736837dde0
Pritamn11 <pritamnarwade11@gmail.com>
2024-02-12 19:26:41 +0530

    Array Added

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,48 @@
+// Dates 
+
+let myDate = new Date()    // Date object/instance
+// console.log(myDate)   // 2024-02-10T12:11:25.440Z
+// console.log(myDate.toString());  // Sat Feb 10 2024 17:43:58 GMT+0530 (India Standard Time)
+// console.log(myDate.toDateString()); // Sat Feb 10 2024
+// console.log(myDate.toISOString());   // 2024-02-10T12:15:18.675Z
+// console.log(myDate.toLocaleString());   // 2/10/2024, 5:45:18 PM
+// console.log(myDate.toJSON());       // 2024-02-10T12:17:28.920Z
+
+// console.log(typeof myDate);    // remember its a object
+
+// let myCreatedDate = new Date(2024, 0, 10)      // month starts from 0, 0 means jan
+// console.log(myCreatedDate);    // 2024-01-09T18:30:00.000Z
+// console.log(myCreatedDate.toDateString());   // Wed Jan 10 2024 
+
+// let myCreatedDate1 = new Date(2024, 0, 10, 5 ,3)
+// console.log(myCreatedDate1); 
+// console.log(myCreatedDate1.toLocaleString());   // 1/10/2024, 5:03:00 AM   outputs with date 
+
+// let myCreatedDate2 = new Date("01-14-2024")  // mm-dd-yyyy format , u can change it
+// console.log(myCreatedDate2);
+// console.log(myCreatedDate2.toLocaleString());    // 1/14/2024, 12:00:00 AM
+
+
+// let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp)  //  1707626431299 from Date.now() represents the number of milliseconds that have passed since a specific starting point in time, which is called the Unix epoch. This starting point is set to January 1, 1970, at midnight UTC (Coordinated Universal Time).
+// console.log(myCreatedDate2.getTime());  // 1705170600000
+
+// console.log(Math.floor(Date.now() / 1000));  // convert a timestamp from milliseconds to seconds.
+
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth()); // 1 mean feb
+// console.log(newDate.getMonth() + 1); // 2 , i make it +1 so that end user wn't confuse
+// console.log(newDate.getFullYear());
+// console.log(newDate.getDay());   // 0 means sunday
+
+const weekday = newDate.toLocaleString('default',{
+    weekday : "long",
+    month : "long",
+    
+})
+ 
+console.log(weekday);   // February Sunday, because today is sunday
+
---
+++ 02_basics/01_array.js
@@ -0,0 +1,51 @@
+//# Array 
+
+let myArr = [1,2,3,4,5]   // inspect this 
+
+let b = "Spider man"
+
+const myArr2 = new Array(1,2,3,4,b)   // inspect this
+// console.log(myArr2);     // [ 1, 2, 3, 4, 'Spider man' ]
+// console.log(myArr2[1])   // 2 
+
+
+//# Array methods 
+// myArr2.push(10)   // [ 1, 2, 3, 4, 'Spider man', 10 ]
+// myArr2.push(7)    // [ 1, 2, 3, 4, 'Spider man', 10, 7 ]
+// myArr2.pop()     // in pop() it didn't take any value, it will delete last value
+// myArr2.unshift(25)  // it add mention element on starting
+// myArr2.shift()  // shift() it didn't take any value,it deletes starting element
+// console.log(myArr2);   
+
+// console.log(myArr2.includes(4));  // true, it includes 4
+// console.log(myArr2.includes(25));  // false, it not includes 25
+
+// console.log(myArr2.indexOf(4))   // 3
+
+// const newArr = myArr2.join()
+
+// console.log(newArr);  // 1,2,3,4,Spider man 
+// console.log(typeof newArr); // its string 
+
+// ## Slice ,Splice 
+
+// The slice() method returns a shallow copy of a portion of an array 
+// into a new array. It does not modify the original array.
+
+// console.log("A", myArr2);   // A [ 1, 2, 3, 4, 'Spider man' ]
+
+// const myA1 = myArr2.slice(0,3)
+// console.log(myA1);    // [ 1, 2, 3 ]
+// console.log(myArr2);   // (original array remains unchanged)
+
+
+// The splice() method changes the contents of an array by removing or replacing existing elements 
+// and/or adding new elements in place. It modifies the original array.
+
+console.log("B", myArr2);   // B [ 1, 2, 3, 4, 'Spider man' ]
+const myA2 = myArr2.splice(0,3)  // // Removes elements starting from index 0 (3 elements)
+console.log(myA2);  // Output: [1, 2, 3]
+console.log(myArr2);    // Output: [ 4, 'Spider man' ] (original array modified)
+
+
+
