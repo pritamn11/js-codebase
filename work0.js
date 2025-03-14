6cfaf194c193b63e85a5fe85939cabdbc795605e
pritamn11 <pritamnarwade11@gmail.com>
2025-03-14 13:34:00 +0530

    work.js added

--- work0.js
+++ work0.js
@@ -1,6 +1,6 @@
-let a = 10;
-let b = a; // b gets a copy of a
-b = 20;    // Changing b does not affect a
 
-console.log(a); // 10
-console.log(b); // 20
+welcomeMessage =  function() {
+    console.log(`Pritam , welcome to javascript world`)
+}
+
+welcomeMessage()
